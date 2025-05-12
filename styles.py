fc0dc76c528fe7ccdb0344cd9f42c21366b386b5
AnyaAleks <aalekseeva.volt@mail.ru>
2025-05-12 13:34:51 +0300

    *\\Новая верстка для home.kv

--- styles.py
+++ styles.py
@@ -1,26 +1,19 @@
 from kivy.utils import get_color_from_hex
-
+from kivy.properties import ListProperty
 
 class AppStyles:
-    # Цвета
-    PRIMARY = get_color_from_hex('#6a0dad')  # Фиолетовый
+    # Цветовая палитра
+    PRIMARY = get_color_from_hex('#6a0dad')  # Основной фиолетовый
+    PRIMARY_DARK = get_color_from_hex('#4b077d')  # Тёмный фиолетовый
+    SECONDARY = get_color_from_hex('#9c27b0')  # Акцентный цвет
     WHITE = get_color_from_hex('#ffffff')
     LIGHT_PURPLE = get_color_from_hex('#f3e5ff')
     DARK_TEXT = get_color_from_hex('#333333')
+    CARD_BACKGROUND = get_color_from_hex('#f9f3ff')
+    ACCENT_COLOR = get_color_from_hex('#ff6b6b')  # Для спецпредложений
 
     # Размеры
-    TITLE_SIZE = '32sp'
+    TITLE_SIZE = '28sp'
     SUBTITLE_SIZE = '18sp'
     BUTTON_HEIGHT = 50
-
-    @staticmethod
-    def colored_button():
-        return {
-            'background_normal': '',
-            'background_color': AppStyles.PRIMARY,
-            'color': AppStyles.WHITE,
-            'font_size': '16sp',
-            'size_hint_y': None,
-            'height': AppStyles.BUTTON_HEIGHT
-
-        }
+    CARD_RADIUS = 15
